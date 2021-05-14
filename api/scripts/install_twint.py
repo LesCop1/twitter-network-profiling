7297fc24abeb5f81f17de1b97be14ed660f6c8e9
Mathis <mathisengels@yahoo.fr>
2021-05-14 19:16:17 +0200

    Add requirements.txt

--- api/scripts/install_twint.py
+++ api/scripts/install_twint.py
@@ -1,2 +1,2 @@
 import os
-os.system("python -m pip install --user --upgrade -e git+https://github.com/twintproject/twint.git@origin/master#egg=twint")
+# os.system("python -m pip install --user --upgrade -e git+https://github.com/twintproject/twint.git@origin/master#egg=twint")
